c3d809946558bf30befb96a3234cee153792d3c2
Richard Kiss <him@richardkiss.com>
2021-03-09 13:38:54 -0800

    Checkpoint. Yikes.

---
+++ src/py/native_view.rs
@@ -0,0 +1,18 @@
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyTuple};
+
+use crate::allocator::Allocator;
+use crate::int_allocator::IntAllocator;
+
+use super::py_int_allocator::PyIntAllocator;
+
+pub struct NativeView {
+    pub arena: PyObject, // PyCell<PyIntAllocator>,
+    pub ptr: <IntAllocator as Allocator>::Ptr,
+}
+
+impl NativeView {
+    pub fn new(arena: PyObject, ptr: <IntAllocator as Allocator>::Ptr) -> Self {
+        NativeView { arena, ptr }
+    }
+}
